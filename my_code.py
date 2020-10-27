b09acd7ce4c2a96470cae31542ba6b500673e632
parkerS123 <23pschwantes@athenian.org>
2020-10-27 21:07:35 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -2,12 +2,13 @@
 # Megan
 # A note on style: Dictionaries can be defined before or after functions.
 
-tictactoe = {'TL': '', 'TM': '', 'TR': '', 'ML': '', 'MM': '', 'MR': '', 'BL': '', 'BM': '', 'BR': '',}
-ok_moves = ['TL', 'TM', 'TR', 'ML', 'MM', 'MR', 'BL', 'BM', 'BR']
+tictactoe = {'TL': '', 'TM': '', 'TR': '', 'ML': '', 'MM': '', 'MR': '', 'BL': '', 'BM': '', 'BR': '',} #my dictionary of moves
+ok_moves = ['TL', 'TM', 'TR', 'ML', 'MM', 'MR', 'BL', 'BM', 'BR'] #moves the user is allowed to make
 
 print("This is a tictactoe game! Someone will be x's and someone will be o's.")
+print("X's goes first!") 
 
-def tictactoe_function(tictactoe): 
+def tictactoe_function(tictactoe): #function that creates the board
     print(tictactoe['TL'] + " |" + tictactoe['TM'] + " | " + tictactoe["TR"])
     print("-+-+-")
     print(tictactoe['ML'] + " |" + tictactoe['MM'] + " | " + tictactoe["MR"])
@@ -15,7 +16,7 @@ def tictactoe_function(tictactoe):
     print(tictactoe['BL'] + " |" + tictactoe['BM'] + " | " + tictactoe["BR"])
 tictactoe_function(tictactoe) 
 
-def three_in_a_row(tictactoe):
+def three_in_a_row(tictactoe): #function that checks if a player won by looking for all possible ways to win
     if tictactoe['TL'] == 'x' and tictactoe['TM'] == 'x' and tictactoe['TR'] == 'x': 
         return True
     elif tictactoe['TL'] == 'o' and tictactoe['TM'] == 'o' and tictactoe['TR'] == 'o':
@@ -50,24 +51,32 @@ def three_in_a_row(tictactoe):
         return True
     else:
         return False
-outcome = three_in_a_row(tictactoe)
-print(outcome)
-moves = 0
+outcome = three_in_a_row(tictactoe) #assigns function above to a variable
+moves = 0 #keeps track of number of moves by both users
 
 while outcome == False: 
-    print("X's goes first!")   
     x_move = input("X's make a move by entering TL, TM, TR, ML, MM, MR, BL, BM, BR: ")
-    o_move = 'nothing'
 
     while True:
         if x_move in ok_moves:
-            ok_moves.remove(x_move)
+            ok_moves.remove(x_move) #removes the move they just made from ok moves so that place can't be plsyed again
             tictactoe[x_move] = 'x' 
             break
         else: 
             print("Please enter a valid move next time...")
             x_move = input("make a move by entering TL, TM, TR, ML, MM, MR, BL, BM, BR: ")
-    tictactoe_function(tictactoe)
+    tictactoe_function(tictactoe) #prints the board
+    outcome = three_in_a_row(tictactoe) #reassigns outcome to check if outcome is now true
+    moves = moves + 1 #adds a move
+    if moves == 9 and outcome == False: #if there are 9 moves and nobody has won then it is a tie
+        print("It's a tie!")
+        tictactoe_function(tictactoe)
+        break
+
+    if outcome == True: #if outcome is now true that means somebody won
+        print("Game Over!")
+        tictactoe_function(tictactoe)
+        break
 
     o_move = input("O's make a move by entering TL, TM, TR, ML, MM, MR, BL, BM, BR: ")
 
@@ -80,17 +89,15 @@ while outcome == False:
             print("Please enter a valid move next time...")
             o_move = input("make a move by entering TL, TM, TR, ML, MM, MR, BL, BM, BR: ")
     tictactoe_function(tictactoe)
-
+    outcome = three_in_a_row(tictactoe)
+    moves = moves + 1
+    
     if outcome == True:
         print("Game Over!")
         tictactoe_function(tictactoe)
         break
-
-    moves = moves + 1
+    
     if moves == 9 and outcome == False:
         print("It's a tie!")
         tictactoe_function(tictactoe)
-
-
-# print(tictactoe)
-tictactoe_function(tictactoe)
+        break
